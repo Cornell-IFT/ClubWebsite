b1025b8e688fc9e2cec5b18e3cbe899d85c302f6
samanthavaca <63178250+samanthavaca@users.noreply.github.com>
2024-09-07 13:50:04 -0500

    Update Projects.js for clubfest 2024

--- src/pages/Projects.js
+++ src/pages/Projects.js
@@ -5,8 +5,8 @@ import ReactPlayer from 'react-player'
 const Projects = () => {
   return (
     <div>
-      <div class="py-56 flex flex-col justify-center 
-                items-center bg-gray-200">
+      <div class="py-40 flex flex-col justify-center 
+                items-center bg-gray-200" style={{ fontFamily: "'Courier New', Courier, monospace" }}>
         <ReactPlayer url="https://youtu.be/JeT6IlrtLf8" />
         <h1 class="w-full mb-10 mt-20 text-4xl font-bold leading-tight text-primary text-center">
           Sit tight
