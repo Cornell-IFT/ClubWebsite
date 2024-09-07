63ed63fa0612936c3a7b43976c7aaf302b4d441c
samanthavaca <63178250+samanthavaca@users.noreply.github.com>
2024-09-07 13:57:40 -0500

    fixed TeamCard.js

--- src/components/TeamCard.js
+++ src/components/TeamCard.js
@@ -1,61 +1,22 @@
-.heading {
-  margin-top: 0px;
-  font-family: 'Courier New', Courier, monospace;
-  font-size: 40px;
-  font-weight: 400;
+import React, { useState } from 'react'
+import './TeamCard.css'
+
+
+function TeamCard(props) {
+  return (
+    <>
+      <div style={{ maxWidth: "16rem" }} class="rounded overflow-hidden shadow-lg w-full">
+        <img class="object-cover h-56 w-full" src={props.image} />
+        <div class="px-6 py-4">
+          <div class="text-center font-bold text-xl mb-2">{props.name}</div>
+          <p class="text-gray-700 text-base text-center">
+            {props.description}
+          </p>
+        </div>
+      </div>
+    </>
+  )
+
 }
 
-.column,
-.column-right {
-  padding-top: 20px;
-  padding-bottom: 20px;
-  width: 50%;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  flex-grow: 1;
-}
-
-.row {
-  display: flex;
-  align-items: center;
-  margin-bottom: 20px;
-  background-color: #d3d3d3;
-  box-shadow: 0px 4px 6px rgba(0, 0, 0, 0.1);
-  padding: 20px;
-  font-family: 'Courier New', Courier, monospace;
-}
-
-.imgbox,
-.imgbox-right {
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  width: 100%;
-  max-width: 400px;
-  height: 300px;
-  overflow: hidden;
-}
-
-.drone-home {
-  width: 100%;
-  height: 100%;
-  object-fit: cover;
-  object-position: center;
-}
-
-.textbox {
-  margin: 0px 20px; 
-}
-
-@media (max-width: 768px) {
-  .row {
-    flex-direction: column;
-    text-align: center;
-  }
-  .column,
-  .imgbox,
-  .imgbox-right {
-    width: 100%;
-  }
-}
+export default TeamCard;
