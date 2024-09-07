e710fdd02abac05955c1a432ce9b6c0fca00a16e
samanthavaca <63178250+samanthavaca@users.noreply.github.com>
2024-09-07 13:49:46 -0500

    Update Home.js for clubfest 2024

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,28 +4,25 @@ import TwoColumn from '../components/TwoColumn';
 
 const Home = () => {
   return (
-    <div>
-      <div class="bg-gray-300">
-        <div class="py-10 flex flex-col justify-center 
-                items-center">
-          <h1 class="w-full mb-10 mt-0 text-4xl font-bold leading-tight text-primary text-center">
-            About
-          </h1>
-          <p class="w-full mb-4 text-lg font-normal text-gray-800 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-800 px-10">
-            Established in 2022, the Innovative Film & Technology Club at Cornell is a team focused on developing new experiences by combining traditional film techniques with modern technology. We have drones, VR sets, and studio equipment to use in a variety of creative endeavours. Have an idea for a cool project? We may be able to help!
-          </p>
-          <p class="w-full mb-10 text-lg font-normal text-gray-800 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-800 px-10">
-            This organization is a registered student organization of Cornell University.
-          </p>
-          <a type="button" class="bg-white hover:bg-gray-100 text-gray-800 font-semibold py-2 px-4 border border-gray-400 rounded shadow" href="https://docs.google.com/forms/d/e/1FAIpQLSdpKWzh4FvQ5V9dtojtDyQbZu5nmFAFbZfOiog9KVH6pBuzwQ/viewform">
+    <div className="bg-[rgb(50,50,50)] text-white py-16 pb-24" style={{ fontFamily: "'Courier New', Courier, monospace" }}>
+      <div className="max-w-7xl mx-auto px-8 pb-24">
+        <h1 className="text-4xl font-bold mb-6 text-center">About</h1>
+        <p className="text-lg leading-relaxed mb-4 text-center">
+          Established in 2022, the Cornell Filmmaking Society is a team focused on developing new experiences by combining traditional film techniques with modern technology. We have drones, VR sets, and studio equipment to use in a variety of creative endeavors. Have an idea for a cool project? We may be able to help!
+        </p>
+        <p className="text-lg leading-relaxed mb-8 text-center">
+          This organization is a registered student organization of Cornell University.
+        </p>
+        <div className="text-center">
+          <a href="https://docs.google.com/forms/d/e/1FAIpQLSdpKWzh4FvQ5V9dtojtDyQbZu5nmFAFbZfOiog9KVH6pBuzwQ/viewform" className="inline-block bg-white text-black font-semibold py-2 px-6 rounded-full hover:bg-gray-200 shadow-md transition duration-300">
             Apply to our team
           </a>
         </div>
       </div>
-      <div class="bg-gray-200">
-        <TwoColumn heading="Our Mission" text="Our mission is to explore ways to combine interests in film and technology. We want to bring stories alive through innovative filmmaking, including but not limited to the use of technologies such as virtual reality, augmented reality, 360 film, and drone usage." image="/drone.jpeg" left={true} />
-        <TwoColumn heading="Our Tools" text="We use anything from drones, VR sets, to post-production software to build out our ideas. We are also open to explore all kinds of computer graphics engines, such as Blender and Unity, to create projects." image="/donut_blender.png" left={false} />
-        <TwoColumn heading="Our Projects" text="As a project-oriented team, we seek to create new experiences every semester. For Spring 2024, our in-house project revolves around filming action scenes." image="/wonder_woman.png" left={true} />
+      <div className="bg-[rgb(210,210,210)] text-black py-16 pb-24" style={{ fontFamily: "'Courier New', Courier, monospace" }}>
+        <TwoColumn heading="Our Mission" text="Our mission is to explore ways to combine interests in film and technology. We want to bring stories alive through innovative filmmaking, including but not limited to the use of technologies such as virtual reality, augmented reality, 360 film, and drone usage." image="/camera_720.png" left={true} />
+        <TwoColumn heading="Our Tools" text="We use anything from drones, VR sets, to post-production software to build out our ideas. We are also open to explore all kinds of computer graphics engines, such as Blender and Unity, to create projects." image="/lighting_720.png" left={false} />
+        <TwoColumn heading="Our Projects" text="As a project-oriented team, we seek to create new experiences every semester." image="/interrogation_720.jpg" left={true} />
       </div>
     </div>
   );
